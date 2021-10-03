0b11ebd015fab3cdcd2ebe6af1b5f7d0dc542d00
Brian OConnor <boconnor20@gmail.com>
2021-10-03 12:01:04 -0500

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/Budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget", 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useNewUrlParser: true,
